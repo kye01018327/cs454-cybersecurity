455eaaadddca2fbbb6a53f2122560d9f21b5e891
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-19 20:58:50 -0800

    chore: init AES.py structure

---
+++ hw2/AES.py
@@ -0,0 +1,31 @@
+
+
+# Functions
+def sub_bytes(input_block: list[list[int]]) -> list[list[int]]:
+    # Accept a 4 by 4 matrix representing the state
+    pass
+
+def shift_rows():
+    pass
+
+def mix_columns():
+    pass
+
+def add_round_key():
+    pass
+
+def key_expansion():
+    pass
+
+def aes_encrypt():
+    pass
+
+def aes_test():
+    pass
+
+def demo_avalanche_effect():
+    pass
+
+def aes_decrypt():
+    pass
+
