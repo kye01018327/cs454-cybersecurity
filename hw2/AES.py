e07d304c4fca65513a460122a72c72654b072375
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-20 00:13:02 -0800

    chore: change wording from block to matrix

--- hw2/AES.py
+++ hw2/AES.py
@@ -6,11 +6,11 @@ AES functions
 """
 
 
-def sub_bytes(input_block: list[list[int]]) -> list[list[int]]:
+def sub_bytes(input_matrix: list[list[int]]) -> list[list[int]]:
     # Accept a 4 by 4 matrix representing the state
     # Use the AES S-box to substitute each byte in the state
     transformed_block = []
-    for row in input_block:
+    for row in input_matrix:
         output_row = []
         for word in row:
             # Use left and right bytes for selecting rows and columns
@@ -21,23 +21,23 @@ def sub_bytes(input_block: list[list[int]]) -> list[list[int]]:
     return transformed_block
 
 
-def shift_rows(input_block: list[list[int]]) -> list[list[int]]:
+def shift_rows(input_matrix: list[list[int]]) -> list[list[int]]:
     # Accept a 4 by 4 matrix state matrix as input
     transformed_block = []
     # Shift the rows as per AES specification
     # Leave the 1st row of State unaltered
     
-    first_row = input_block[0]
+    first_row = input_matrix[0]
     # Circular left shift 1 time for 2nd row
-    second_row = input_block[1]
+    second_row = input_matrix[1]
     second_row = second_row[1:] + second_row[:1]
     
     # Circular left shift 2 times for 3rd row
-    third_row = input_block[2]
+    third_row = input_matrix[2]
     third_row = third_row[2:] + third_row[:2]
 
     # Circular left shift 3 times for 4th row
-    fourth_row = input_block[3]
+    fourth_row = input_matrix[3]
     fourth_row = fourth_row[3:] + fourth_row[:3]
 
     # Construct transformed block
@@ -48,14 +48,14 @@ def shift_rows(input_block: list[list[int]]) -> list[list[int]]:
 
     return transformed_block
 
-def mix_columns(input_block: list[list[int]]) -> list[list[int]]:
+def mix_columns(input_matrix: list[list[int]]) -> list[list[int]]:
     # Accept a 4 by 4 state matrix as input
     # Multiply each column of the state by a fixed polynomial matrix in the Galois Field (GF(2^8))
-    for column in zip(*input_block):
+    for column in zip(*input_matrix):
         pass
     
 
-def add_round_key():
+def add_round_key(input_matrix: list[list[int]], key_matrix):
     pass
 
 
--- hw2/utils.py
+++ hw2/utils.py
@@ -39,9 +39,9 @@ def printb(block: list[list[int]]) -> None:
     print()
 
 
-def convert_int_to_block(input: int) -> list: 
+def convert_int_to_matrix(input: int) -> list: 
     total_bytes = 16 
-    block = []
+    matrix = []
     for i in range(4):
         row = []
         for j in range(4):
@@ -49,15 +49,15 @@ def convert_int_to_block(input: int) -> list:
             shift_amount = byte_index * 8
             byte = (input >> shift_amount) & 0xff
             row.append(byte)
-        block.append(row)
-    return block
+        matrix.append(row)
+    return matrix
 
-def convert_block_to_int(block: list[list[int]]) -> int:
+def convert_matrix_to_int(matrix: list[list[int]]) -> int:
     total_bytes = 16 
     output_int = 0
     for i in range(4):
         for j in range(4):
-            byte = block[i][j]
+            byte = matrix[i][j]
             byte_index = total_bytes - 1 - (i * 4 + j)
             shift_amount = byte_index * 8
             output_int |= (byte << shift_amount)
