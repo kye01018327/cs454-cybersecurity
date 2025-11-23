128aaa7b92b8d6a820862c9c2cc5a7f3f7536e85
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-22 17:04:06 -0800

    chore: change plaintext and key

--- hw2/utils.py
+++ hw2/utils.py
@@ -1,6 +1,6 @@
 # Test data
-plaintext = 0x3243f6a8885a308d313198a2e0370734
-key = 0x2b7e151628aed2a6abf7158809cf4f3c
+plaintext = 0x00112233445566778899aabbccddeeff
+key = 0x000102030405060708090a0b0c0d0e0f
 
 
 # AES tables
