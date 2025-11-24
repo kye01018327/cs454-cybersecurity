5e6cf74d28dc47955124b71022efc4383b036bba
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-24 14:10:25 -0800

    chore: delete

--- hw2/test_aes_encryption.py
+++
@@ -1,9 +0,0 @@
-from aes import *
-from utils import *
-
-def test_aes_encryption():
-    textbook_plaintext = 0x0123456789abcdeffedcba9876543210
-    textbook_key = 0x0f1571c947d9e8590cb7add6af7f6798
-    textbook_ciphertext = 0xff0b844a0853bf7c6934ab4364148fb9
-    ciphertext = aes_encrypt(textbook_plaintext, textbook_key)
-    assert ciphertext == textbook_ciphertext
